f0227b4e79251336e94b00414900823b9c69a740
lucia nambi <nambilucia@gmail.com>
2024-03-30 20:16:23 +0300

    add all changes

---
+++ index.js
@@ -0,0 +1,30 @@
+let container=document.querySelector(".container");
+
+function redColor(){
+let redBox=document.getElementById("red-box");
+redBox.style.background="red";
+ container.style.background="red";
+  };
+ 
+
+function greenColor(){
+let greenBox=document.getElementById("green-box");
+greenBox.style.background="green";
+container.style.background="green";
+} 
+
+function blueColor(){
+let blueBox=document.getElementById("blue-box");
+blueBox.style.background="blue";
+container.style.background="blue";
+} 
+
+function whiteColor(){
+let whiteBox=document.getElementById("white-box");
+whiteBox.style.background="white";
+container.style.background="white";
+} 
+
+ 
+
+
